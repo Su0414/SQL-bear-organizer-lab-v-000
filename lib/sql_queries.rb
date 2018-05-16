47463cb631518c50536de93729f84eadef273584
Suchitra Naik <susanaik@gmail.com>
2018-05-16 17:38:20 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -34,7 +34,7 @@ end
 
 def counts_number_of_bears_with_goofy_temperaments
   "Write your SQL query here"
-  "select * from bears where temperament = 'goofy'"
+  "select count(*) from bears where temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
