9450d9bb43709e52f9a4c1356d364d36fb13528b
Suchitra Naik <susanaik@gmail.com>
2018-05-16 17:51:56 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,11 +29,11 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
 
-  "select color, count(color) from bears"
+  "select color, max(color) from bears"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  
   "select count(*) from bears where temperament = 'goofy'"
 end
 
