105669f01b6850023df2a3c33ef539d9c5422a48
Suchitra Naik <susanaik@gmail.com>
2018-05-16 17:09:39 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,32 @@
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Mr Chocolate', 10,'M','brown','quiet','1');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Rowdy', 10,'M','brown','quiet','1');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Tabitha', 5,'F','brown','quiet','1');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Sergeant Brown', 10,'M','black','grizzly','1');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Melissa', 10,'M','brown','angry','1');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Grinch', 10,'M','brown','quiet','1');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Wendy', 10,'M','brown','quiet','1');
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 10,'M','brown','quiet','0');
