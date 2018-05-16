69b824bec2e728462838ca4346d12f92a78baf94
Suchitra Naik <susanaik@gmail.com>
2018-05-16 17:30:13 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,7 +11,7 @@ end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 
-  "select name, age from bears where alive ='1' AND order by age"
+  "select name, age from bears where alive ='1' AND order by age asc"
 
 end
 
