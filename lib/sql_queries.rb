6c3966e92bdc99f46f78b8eb291f0001ee0d4466
Suchitra Naik <susanaik@gmail.com>
2018-05-16 17:20:32 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -5,22 +5,32 @@ end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
   "Write your SQL query here"
+
+  "select name from bears order by name asc"
+
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
   "Write your SQL query here"
+  "select name, age from bears where alive ='1' AND order by age"
+
 end
 
 def selects_oldest_bear_and_returns_name_and_age
   "Write your SQL query here"
+  "select name, age from bears order by age desc limit 1"
+
 end
 
 def select_youngest_bear_and_returns_name_and_age
   "Write your SQL query here"
+  "select name, age from bears order by age asc limit 1"
+
 end
 
 def selects_most_prominent_color_and_returns_with_count
   "Write your SQL query here"
+
 end
 
 def counts_number_of_bears_with_goofy_temperaments
